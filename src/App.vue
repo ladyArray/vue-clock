01d1c4f433a4af4e0c2db83c01267239bde7b19a
Regina Rguez <regina.rguez.c@gmail.com>
2022-05-18 16:29:34 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+
+<script>
+import VueClock from "./components/VueClock.vue"
+export default{
+name: "App",
+components: {
+  VueClock
+}
+}
+
+</script>
+
+<template>
+
+<VueClock>
+
+</VueClock>
+
+
+
+
+</template>
+
+<style>
+
+
+</style>
+
---
+++ src/components/VueClock.vue
@@ -0,0 +1,28 @@
+
+<script>
+export default
+{name: "VueClock",
+data (){
+  return{
+    hours: new Date().getHours(),
+    minutes: new Date().getMinutes(),
+    seconds: new Date().getSeconds()
+  }
+},
+
+}
+</script>
+
+<template>
+<div>
+  <span class="hours">{{hours}}:</span>
+  <span class="minutes">{{minutes}}:</span>
+  <span class="seconds">{{seconds}}</span>
+</div>
+
+</template>
+
+<style>
+
+</style>
+
